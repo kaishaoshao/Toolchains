0b1bfed361e4b34032fd6ff781260197a8f6864b
kaishaoshao <2032751077@qq.com>
2025-07-27 01:58:09 +0800

    [kdb] 创建用户界面
    Finish: after kdb -p xxx
    
    kdb>

--- src/kdb/tools/kdb.cpp
+++ src/kdb/tools/kdb.cpp
@@ -6,7 +6,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
 namespace {
+    // 附加到一个进程
+    // 如果传入的参数是 -p <pid>，则附加到指定的 PID 进程
+    // 如果传入的参数是 <program_path>，则创建一个新进程并执行该程序
+    // 返回附加的 PID 或新创建的 PID
+    // 如果附加失败，返回 -1
     pid_t attach(int argc, char **argv){
         pid_t pid = 0;
         // Passing PID 
@@ -49,6 +57,9 @@ namespace {
         }
         return pid;
     }
+
+    // 添加用户界面
+    void handle_command(pid_t pid, std::string_view line) {}
 }
 
 int main(int argc, char **argv){
@@ -68,6 +79,32 @@ int main(int argc, char **argv){
         std::perror("Waitpid failed");
     }
 
-
+    char *line = nullptr;
+    // 使用 readline 库来处理用户输入
+    // readline 会自动处理输入的历史记录
+    // while((line = realine("kdb> ") != nullptr) {
+    //     handle_command(pid, line);
+    //     add_history(line);
+    //     free(line);
+    // } 
+    while((line = readline("kdb> ")) != nullptr)
+    {
+        std::string line_str;
+        if(line == std::string_view("")) 
+        {
+            free(line);
+            if(history_length > 0) 
+                line_str = history_list()[history_length - 1]->line;
+        }  
+        else
+        {
+            line_str = line;
+            add_history(line);
+            free(line);
+        }
+        if(!line_str.empty())
+            handle_command(pid, line_str);
+    }
+    
     return 0;
 }
