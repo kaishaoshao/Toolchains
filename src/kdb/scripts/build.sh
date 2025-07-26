fbb6a447bcb52dc21e556b700ed19732615cede1
kaishaoshao <2032751077@qq.com>
2025-07-27 01:06:50 +0800

    [kdb] 启动和附加进程
    Finish: 1. kdb <program name> 2. kdb -o <pid>

--- src/kdb/scripts/build.sh
+++ src/kdb/scripts/build.sh
@@ -2,7 +2,7 @@
 scripts_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 mkdir -p build
 mkdir -p third
-third_dir=${scripts}/../third
+third_dir=${scripts_dir}/../third
 
 if [ ! -d ${third}/vcpkg ]; then
     git clone https://github.com/microsoft/vcpkg.git ${third_dir}/vcpkg
@@ -10,5 +10,5 @@ fi
 
 cmake -S . -B build  \
 -DCMAKE_TOOLCHAIN_FILE=${third_dir}/vcpkg/scripts/buildsystems/vcpkg.cmake && cmake --build build 
-./build/tools/kdb
+./build/tools/kdb -p 
 ./build/test/tests
--- src/kdb/tools/kdb.cpp
+++ src/kdb/tools/kdb.cpp
@@ -1,6 +1,73 @@
-#include <libkdb/libkdb.hpp>
+#include <iostream>
+#include <unistd.h>
+
+#include <string_view>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+namespace {
+    pid_t attach(int argc, char **argv){
+        pid_t pid = 0;
+        // Passing PID 
+        if (argc == 3 && argv[1] == std::string_view("-p")) {
+            pid = std::stoi(argv[2]);
+            if (pid < 0)
+            {
+                std::cerr << "Invalid PID" << std::endl;
+                return -1;
+            }
+            
+            if(ptrace(PTRACE_ATTACH, pid, /*addr*/nullptr, /*data*/nullptr) < 0) {
+                std::perror("Could not attach");
+            }
+        }
+        // Passing process name
+        else {
+            const char *program_path = argv[1];
+            if ((pid = fork()) < 0)
+            {   
+                std::perror("Fork failed");
+                return -1;
+            }
+            if (pid == 0)
+            {
+                // We're in the child process
+                // Execute debugee.
+                // 如果我们处于子进程，我们应该用我们想要调试的程序替换当前正在执行的程序
+                // 在我们exec之前，我们必须先调用ptrace(PTRACE_TRACEME)将程序设置为可调试的
+                if(ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
+                    std::perror("Could not set trace");
+                    return -1;
+                } 
+                if(execlp(program_path, program_path, nullptr) < 0) {
+                    std::perror("Exec failed");
+                    return -1;
+                }
+            }
+            
+        }
+        return pid;
+    }
+}
 
 int main(int argc, char **argv){
-    kdb::say_hello();
+
+    if(argc == 1) {
+        std::cerr << "No arguments given\n";
+        return 1;
+    }
+
+    pid_t pid = attach(argc, argv);
+
+    // 在附加进程后，等待其暂停执行，接受用户输入
+    int wait_status;
+    int options = 0;
+    if(waitpid(pid, &wait_status, options) < 0) 
+    {
+        std::perror("Waitpid failed");
+    }
+
+
     return 0;
 }
