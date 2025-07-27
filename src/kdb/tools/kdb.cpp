6dfd4c5ca5e8f93440bb2c7893e76257d61d8ef6
kaishaoshao <2032751077@qq.com>
2025-07-27 15:49:10 +0800

    [kdb] 处理用户输入

--- src/kdb/tools/kdb.cpp
+++ src/kdb/tools/kdb.cpp
@@ -9,6 +9,10 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
 namespace {
     // 附加到一个进程
     // 如果传入的参数是 -p <pid>，则附加到指定的 PID 进程
@@ -58,8 +62,63 @@ namespace {
         return pid;
     }
 
-    // 添加用户界面
-    void handle_command(pid_t pid, std::string_view line) {}
+    // 添加用户界面 && 处理用户输入
+
+    // 将输入的字符串按指定分隔符分割成多个子字符串
+    std::vector<std::string> split(std::string_view str, char delimiter) {
+        std::vector<std::string>  out{};
+        std::stringstream ss{std::string(str)};
+        std::string item;
+
+        while(std::getline(ss, item, delimiter)) 
+            out.push_back(item);
+        return out;
+    }
+
+    // 检查一个字符串是否是另一个字符串的前缀
+    bool is_prefix(std::string_view str, std::string_view of) {
+        if(str.size() > of.size())
+            return false;
+        return std::equal(str.begin(), str.end(), of.begin());
+    }
+
+    // 恢复进程的执行
+    // 通过 PTRACE_CONT 命令让进程继续执行
+    // 如果恢复失败，打印错误信息并退出程序
+    void resume(pid_t pid) {
+        if(ptrace(PTRACE_CONT, pid, /*addr*/nullptr, /*data*/nullptr) < 0)
+        {
+            std::perror("Could not continue\n");
+            std::exit(-1);
+        }
+    }
+
+    // 等待进程发送信号
+    void wait_on_signal(pid_t pid) 
+    {
+        int wait_status;
+        int options =0;
+        if(waitpid(pid, &wait_status, options) < 0)
+        {
+            std::perror("Waitpid failed");
+            std::exit(-1);
+        }
+    }
+
+    // 处理用户输入的命令
+    void handle_command(pid_t pid, std::string_view line) 
+    {
+        auto args = split(line, ' ');
+        auto command = args[0];
+        if(is_prefix(command, "continue")) 
+        {
+            resume(pid);
+            wait_on_signal(pid);
+        }
+        else 
+            std::cerr << "Unknown command\n";
+
+    }
 }
 
 int main(int argc, char **argv){
