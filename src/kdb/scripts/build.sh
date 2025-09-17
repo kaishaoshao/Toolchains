50d203bdb7c7f7f5b9d3a060a52ed61df60621d2
kaishaoshao <2032751077@qq.com>
2025-09-18 00:08:29 +0800

    TODO

--- src/kdb/scripts/build.sh
+++ src/kdb/scripts/build.sh
@@ -10,5 +10,12 @@ fi
 
 cmake -S . -B build  \
 -DCMAKE_TOOLCHAIN_FILE=${third_dir}/vcpkg/scripts/buildsystems/vcpkg.cmake && cmake --build build 
-./build/tools/kdb -p 
+
+# 处理用户输入
+# sudo bash -c 'echo 0 > /proc/sys/kernel/yama/ptrace_scope'
+# $  while sleep 10; do echo "I'm alive!"; done&
+# $  ./build/tools/kdb -p 1460373 
+# $ setcap CAP_SYS_PTRACE+eip ./build/tools/kdb
+
+./build/tools/kdb 
 ./build/test/tests
